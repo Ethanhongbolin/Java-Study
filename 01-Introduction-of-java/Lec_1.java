3c1a0f2829dd218011fa80dd67ae0be3ccc17dd0
ethanhongbolin <ethan.linhongbo@gmail.com>
2026-08-05 12:24:46 -0700

    Introduction

---
+++ 01-Introduction-of-java/Lec_1.java
@@ -0,0 +1,10 @@
+
+// 01 - 
+public class Lec_1 {
+    public static void main(String[] args) {
+        System.out.println("Name:\tAlex");
+        System.out.println("She \nsaid, \"Hello.\"");
+        System.out.println("C:\\study\\java");
+    }
+}
+
